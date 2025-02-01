559637c7e2a6b464cd774795f81af22a9ad3a8fa
SadeeshaRandina <sadee.randina25@gmail.com>
2025-02-01 19:54:06 +0530

    Added form 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,31 @@
-import Pregnancy from "./Pregnancy";
+import Form2 from "./Pregnancy";
 import Tables from "./Tables";
 import Table2 from "./Table2";
+import Form1 from "./Form1";
+import { BrowserRouter as Router, Route, Routes, useNavigate} from "react-router-dom";
 
-function App (){
-  return(
-        <>
-          <Pregnancy/>
-          
-        
-        </>
-          
+const Home = () => {
+  const navigate = useNavigate();
 
-        );
+  return (
+    <div>
+      <button onClick={() => navigate("/form1")}>Go to Form1</button>
+      <button onClick={() => navigate("/form2")}>Go to Form2</button>
+    </div>
+  );
+};
+function App() {
+  
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/form1" element={<Form1 />} />
+        <Route path="/form2" element={<Form2 />} />
+      </Routes>
+    </Router>
+  );
 }
+
 export default App;
 
