229c10f5aa90846da0229bf2585f6a3440e8554b
ml567 <miyunilowe5@gmail.com>
2025-02-01 22:19:52 +0530

    Moved files into folder frontend

---
+++ frontend/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import { useState } from 'react';
+import Tables from './Tables';
+import './App.css'
+
+const App = ()  =>{
+  return(
+  <Tables></Tables>
+
+);}
+
+export default App
