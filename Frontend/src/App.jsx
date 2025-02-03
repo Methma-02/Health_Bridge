0b22e35c338fba567ce3a5170767f50b5e8beb5a
ml567 <miyunilowe5@gmail.com>
2025-02-03 19:41:58 +0530

    Add chdr babyDetails component

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import ImmunizationForm from './vaccination' 
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <ImmunizationForm/>
+    
+    </>
+   
+  )
+}
+
+export default App
