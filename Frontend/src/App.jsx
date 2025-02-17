84a0ee82503dcd0e871673ab397adab43b728ff6
ml567 <miyunilowe5@gmail.com>
2025-02-17 11:13:06 +0530

    updated App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import Chart from "./developmentMilestones";
+import StudentHealthRecords from "./studentHealthRecords";
 import BabyDetails from "./babyDetails";
 import ImmunizationForm from "./Immunization";
+import DevelopmentMilestones from "./developmentMilestones";
 import HealthRecords from "./childHealthRecord";
-import StudentHealthRecords from "./studentHealthRecords";
+import SensoryScreening from "./scensoryScreening";
+
 
 function App(){
   return(
     <>
+    <BabyDetails/>
+    <ImmunizationForm/>
     <StudentHealthRecords/>
+    <DevelopmentMilestones/>
+    <HealthRecords/>
+    <StudentHealthRecords/>
+    <SensoryScreening/>
     
     </>
   )
