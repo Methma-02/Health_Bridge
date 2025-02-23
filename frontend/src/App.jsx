3c80fa7c7650e8e58ae581e2595ba6982dba978c
ml567 <miyunilowe5@gmail.com>
2025-02-23 15:06:06 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import ImmunizationForm from "./Immunization";
 import DevelopmentMilestones from "./developmentMilestones";
 import HealthRecords from "./childHealthRecord";
 import SensoryScreening from "./sensoryScreening";
+import WeightGainChart from "../WeightChart";
+import HeightGainChart from "../HeightChart";
 
 
 function App(){
@@ -16,6 +18,8 @@ function App(){
     <HealthRecords/>
     <StudentHealthRecords/>
     <SensoryScreening/>
+    <WeightGainChart/>
+    <HeightGainChart/>
     
     </>
   )
