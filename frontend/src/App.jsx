4943bfda16e3301203990006146de95569305d61
ml567 <miyunilowe5@gmail.com>
2025-02-28 12:32:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,10 @@
-import StudentHealthRecords from "./pages/studentHealthRecords";
-import BabyDetails from "./pages/babyDetails";
-import ImmunizationForm from "./pages/Immunization";
-import DevelopmentMilestones from "./pages/developmentMilestones";
-import HealthRecords from "./pages/childHealthRecord";
-import SensoryScreening from "./pages/sensoryScreening";
-import WeightGainChart from "./pages/WeightChart";
-import HeightGainChart from "./pages/HeightChart";
+import ChildHealthRecords from "./pages/childHealthRecord";
 
 
 function App(){
   return(
     <>
-    <BabyDetails/>
-    <ImmunizationForm/>
-    <StudentHealthRecords/> 
-    <DevelopmentMilestones/>
-    <HealthRecords/>
-    <StudentHealthRecords/>
-    <SensoryScreening/>
-    <WeightGainChart/>
-    <HeightGainChart/>
+    <ChildHealthRecords/>
     
     </>
   )
