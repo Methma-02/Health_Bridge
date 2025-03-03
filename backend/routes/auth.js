f57201868c21a2f7421a258b829db10bb2a0bd5a
Methma-02 <146626620+Methma-02@users.noreply.github.com>
2025-03-04 03:35:27 +0530

    added a new router to the autyh.jsx

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,5 +7,7 @@ router.post('/login', validateLogin, AuthController.login);
 router.post('/google', AuthController.googleLogin);
 router.post('/forgot-password', AuthController.forgotPassword);
 router.post('/reset-password', AuthController.resetPassword);
+router.get('/verify-reset-token/:token', AuthController.verifyResetToken);
+
 
 module.exports = router;
