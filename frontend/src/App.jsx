cdb65386150b5e02cd260556cc7befc0fb78d861
Methma-02 <146626620+Methma-02@users.noreply.github.com>
2025-03-09 10:32:20 +0530

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,13 @@ import { AuthProvider } from './authContext';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import ResetPasswordPage from './ResetPasswordPage';
 import ForgotPasswordPage from './ForgotPasswordPage';
+import { RecoveryProvider } from './RecoveryContext'; // Add this import
 
 function App() {
   return (
     <AuthProvider>
       <GoogleOAuthProvider clientId="995536188022-t1bci6di33lak0lfulniv7me90mj172t.apps.googleusercontent.com">
+      <RecoveryProvider>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LandingPage />} />
@@ -21,8 +23,10 @@ function App() {
             <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
             <Route path="*" element={<Navigate to="/" replace />} />
             <Route path="/reset-password/:email" element={<ResetPasswordPage />} />
+            <Route path="/reset-password" element={<ResetPasswordPage />} />
           </Routes>
         </BrowserRouter>
+        </RecoveryProvider>
       </GoogleOAuthProvider>
     </AuthProvider>
   );
