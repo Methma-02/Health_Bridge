56c9aec55128d14da024e68c964bf142a0a29c21
Methma-02 <146626620+Methma-02@users.noreply.github.com>
2025-03-17 20:15:33 +0530

    Create locationService.jsx

---
+++ frontend/src/components/services/locationService.jsx
@@ -0,0 +1,21 @@
+// frontend/src/services/locationService.js
+export const getCurrentPosition = () => {
+    return new Promise((resolve, reject) => {
+      if (!navigator.geolocation) {
+        reject(new Error('Geolocation is not supported by your browser'));
+      } else {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            resolve({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude
+            });
+          },
+          (error) => {
+            reject(error);
+          },
+          { enableHighAccuracy: true }
+        );
+      }
+    });
+  };
