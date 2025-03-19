a2f26871b2b7d81f647f2dfa2dd5b3b794754a03
Methma-02 <146626620+Methma-02@users.noreply.github.com>
2025-03-19 21:24:26 +0530

    Update emergencyService.jsx

--- frontend/src/components/services/emergencyService.jsx
+++ frontend/src/components/services/emergencyService.jsx
@@ -1,7 +1,7 @@
 // frontend/src/services/emergencyService.js
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
 export const createEmergencyAlert = async (userId, latitude, longitude, additionalInfo = '') => {
   try {
