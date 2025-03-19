b77fb8b2bae6b8487492459008b9a03615ec8c05
SadeeshaRandina <sadee.randina25@gmail.com>
2025-03-20 01:21:13 +0530

    Update route for PartB to use the new Pregnancy component path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
-import Form2 from "./Pregnancy";
-import TablesR1 from "./TablesR1";
-import Table2 from "./TableR2";
+import Form2 from "./components/PregnancyPartB/Pregnancy"
 import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Registration from "./components/RegistrationInformation/RegistrationInformation";
 import ClinicCareTables from "./components/ClinicCare/Tables"; 
 import PostnatalCare from "./components/PostnatalCare/PostnatalCare";
 import Refferal from "./components/Refferal/Refferal";
-import Pregnancy from "./Pregnancy";
 
 function App() {
   return (
@@ -18,7 +15,7 @@ function App() {
         <Route path="/clinic-care" element={<ClinicCareTables />} />
         <Route path="/postnatal-care" element={<PostnatalCare />} />
         <Route path="/refferal" element={<Refferal />} />
-        <Route path="/PartB" element={<Pregnancy />} />
+        <Route path="/PartB" element={<Form2 />} />
       </Routes>
     </Router>
   );
