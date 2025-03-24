dbb1d6269dfc7c29b7e6047fdd78df8407c19350
Methma-02 <146626620+Methma-02@users.noreply.github.com>
2025-03-24 07:18:36 +0530

    Update socketHandlers.js

--- backend/socket/socketHandlers.js
+++ backend/socket/socketHandlers.js
@@ -3,12 +3,16 @@ const { Server } = require('socket.io');
 let io;
 
 const setupSocket = (server) => {
+
+  console.log("setting up socket .. ");
   io = new Server(server, {
     cors: {
       origin: process.env.FRONTEND_URL || 'http://localhost:5173',
       methods: ['GET', 'POST']
     }
   });
+
+  console.log("socket created...");
 
   io.on('connection', (socket) => {
     console.log('New client connected:', socket.id);
